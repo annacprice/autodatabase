f786f2fbaeda98c5c3f4a12413c5639964794443
annacprice <PriceA35@cardiff.ac.uk>
2020-09-01 15:54:10 +0000

    add singularity build script

---
+++ singularity/singularity_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+container_list=("kraken2" "mash" "pythonenv")
+software_tag="autoDatabase"
+
+for item in ${container_list[@]}; do
+    sudo singularity build ${software_tag}_${item}.sif Singularity.${software_tag}_${item}
+done
